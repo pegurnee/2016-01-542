1189041d629277aac80a5fe26998e584c5a35020
Eddie Gurnee <pegurnee@gmail.com>
2016-01-13 03:04:10 -0500

    finish tokenizer v1

--- projects/1_project/tokenizer.py
+++ projects/1_project/tokenizer.py
@@ -15,31 +15,16 @@ class Tokenizer:
       for token in lines[x].split():
         self._tokens[x].append(token)
 
+  def append(self, code_string):
+    self._tokenize(code_string)
+
   def next(self):
     if not self._tokens[0]:
       self._tokens.pop(0)
-      
+
     return self._tokens[0].pop(0)
 
   def is_empty(self):
     if not self._tokens:
       return True
-    else:
-      for tokenlist in self._tokens:
-        if not tokenlist:
-          return True
     return False
-
-  def _consume(self, _nomable=None):
-    if _nomable == '$$':
-      return True
-    if not self._tokens:
-      return True
-    elif not self._tokens[0]:
-      self._tokens.pop(0)
-
-    # TODO: add current token to AST
-    self._token = self._tokens[0].pop(0)
-
-  def append(self, code_string):
-    pass
