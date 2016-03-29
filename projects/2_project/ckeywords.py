4eeb35a1533b2e52923041c23d3e8128492ffadf
Eddie Gurnee <pegurnee@gmail.com>
2016-03-29 05:07:21 -0400

    add flow keywords

---
+++ projects/2_project/ckeywords.py
@@ -0,0 +1,5 @@
+keywords = ['auto',
+  'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum', 'extern', 'float', 'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short',
+  'signed', 'sizeof', 'static', 'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while']
+flowkeys = ['case', 'const', 'else', 'extern', 'for', 'goto', 'if', 'return', 'switch', 'union', 'while'
+]
