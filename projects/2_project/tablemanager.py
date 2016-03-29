b1b687674780eb3cb2c0a38138af67d68fc21350
Eddie Gurnee <pegurnee@gmail.com>
2016-03-29 04:17:47 -0400

    add basic test case

--- projects/2_project/tablemanager.py
+++ projects/2_project/tablemanager.py
@@ -8,18 +8,34 @@ class TableManager:
 
   def parse_line(self, line):
     #here
-    if line.find('{')
+    print(line)
+    if line.find('{'):
       self.table.initialize_scope()
 
     words = line.split()
     if words[0] in ['void','int','char']:
-      if not self.table.lookup(words[1]):
-        self.table.insert(words[1])
+      word = words[1]
 
-    if line.find('='):
-      assign_stmt, value = tuple(map(lambda x: x.trim(), line.split('=')))
+      pointer = False
+      more = False
+      if '*' in word:
+        word = word.replace('*', '')
+        pointer = True
+      if ',' in word:
+        word = word.replace(',', '')
+        more = True
 
-    if line.find('}')
+      if not self.table.lookup(word):
+        self.table.insert(word, words[0] if words[0] != 'void' else 'function')
+
+    if '=' in line:
+      assign_stmt, value = tuple(map(lambda x: x.strip(), line.split('=')))
+
+    if line.find('}'):
       self.finalize_scope()
 
 if __name__ == '__main__':
+  man = TableManager()
+  with open('test_cases/p257.c') as f:
+    for line in f:
+      man.parse_line(line)
