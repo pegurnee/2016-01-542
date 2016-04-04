974b2bc6655625c8706aa85b88efd95c87e43356
Eddie Gurnee <egurnee@emich.edu>
2016-04-04 09:21:24 -0400

    move testing out of operational

--- projects/2_project/tablemanager.py
+++ projects/2_project/tablemanager.py
@@ -125,13 +125,4 @@ def main():
   print('=' * 40)
 
 if __name__ == '__main__':
-  test_files = ['p253.c', 'p257.c', 'p267.c', 'p324.c']
-  for fname in test_files:
-    man = TableManager()
-    with open('test_cases/' + fname) as f:
-      for line in f:
-        man.parse_line(line)
-    print(man.table)
-    print('=' * 40)
-
   main()
