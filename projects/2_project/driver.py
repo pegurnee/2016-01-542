f211d753cde95c10e96e3d9ac7854630a22d32bd
Eddie Gurnee <egurnee@emich.edu>
2016-04-06 10:09:06 -0400

    parsing some args

--- projects/2_project/driver.py
+++ projects/2_project/driver.py
@@ -3,21 +3,35 @@ import os, sys, getopt
 from tablemanager import TableManager
 
 def main():
-  if len(sys.argv) == 2:
-    infile = sys.argv[1]
-    outfile = sys.argv[1][ : sys.argv[1].rfind('.') + 1] + 'sym'
-  elif len(sys.argv) == 3:
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
+  verbose = False
+  if len(sys.argv) != 1:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:o:v')
+
+    if len(args) > 0:
+      infile = args[0]
+      outfile = args[0][ : args[0].rfind('.') + 1] + 'sym'
+    if len(args) > 1:
+      outfile = args[1]
+
+    for o,a in opts:
+      if o == '-i':
+        infile = a
+        outfile = infile[ : infile.rfind('.') + 1] + 'sym'
+      elif o == '-o':
+        outfile = a
+      elif o == '-v':
+        verbose = True
+
   else:
     infile = input('enter file name: ')
 
   man = TableManager('test_cases/' + infile)
 
-  print('Your table is served:')
-  print('=' * 40)
-  print(man.table)
-  print('=' * 40)
+  if verbose:
+    print('Your table is served:')
+    print('=' * 40)
+    print(man.table)
+    print('=' * 40)
 
   with open(outfile, 'w') as f:
     f.write(str(man))
