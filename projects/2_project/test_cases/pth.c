e4f0c0f941ca29851d419d2113c6cd1747c0c0ce
Eddie Gurnee <egurnee@emich.edu>
2016-04-11 10:12:49 -0400

    add tyler's sample code

---
+++ projects/2_project/test_cases/pth.c
@@ -0,0 +1,25 @@
+static int w;
+char x;
+float b[10], c;
+int main() {
+  int * c = 2;
+  {
+    static int **b[2][3], z;
+  }
+  {
+    int a, x;
+    {
+      double c, x;
+      int d;
+      int j, i;
+      x = a - a + c / x * 2 % 3 % x + 3;
+      int my_int = 3, other = 4;
+
+      j = my_int;
+    }
+  }
+}
+
+void example2(int x) {
+  int a = 9;
+}
