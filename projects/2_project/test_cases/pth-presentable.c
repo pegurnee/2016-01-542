a8a6b0c2e98f1a4e579d311d02093b4aee52c01f
Eddie Gurnee <egurnee@emich.edu>
2016-04-11 11:30:54 -0400

    difference between usable and USABLE

---
+++ projects/2_project/test_cases/pth-presentable.c
@@ -0,0 +1,27 @@
+static int w;
+char x;
+int b[10];
+int c;
+int main() {
+  int * c = 2;
+  {
+    static int b[2][3];
+    int z;
+  }
+  {
+    int a, x;
+    {
+      char c, x;
+      int d;
+      int j, i;
+      x = a - a + c / x * 2 % 3 % x + 3;
+      int my_int = 3, other = 4;
+
+      j = my_int;
+    }
+  }
+}
+
+void example2(int x) {
+  int a = 9;
+}
--- projects/2_project/test_symtab.py
+++ projects/2_project/test_symtab.py
@@ -1,7 +1,7 @@
 from tablemanager import TableManager
 
 def main():
-  test_files = ['p253.c', 'p257.c', 'p267.c', 'p324.c', 'pth-usable.c']
+  test_files = ['p253.c', 'p257.c', 'p267.c', 'p324.c', 'pth-presentable.c']
   for fname in test_files:
     man = TableManager()
     with open('test_cases/' + fname) as f:
